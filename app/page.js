ccc58ae6e1e4ad35b660b6afb570c62feb3b6d3f
Emilio L. Aleman <emiliolaleman@gmail.com>
2024-08-04 23:18:26 -0400

    change next.js and firebase wording

--- app/page.js
+++ app/page.js
@@ -23,18 +23,19 @@ export default function Home() {
   }
 
   useEffect(() => {
-    updateInventory();
-  }, []);
+    updateInventory()
+  }, [])
 
   const addItem = async (item) => {
     const docRef = doc(collection(firestore, 'inventory'), item);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      const { quantity } = docSnap.data();
-      await setDoc(docRef, { quantity: quantity + 1 });
-    } else {
-      await setDoc(docRef, { quantity: 1 });
+      const {quantity} = docSnap.data();
+      await setDoc(docRef, {quantity: quantity + 1});
+    } 
+    else {
+      await setDoc(docRef, {quantity: 1});
     }
 
     await updateInventory();
@@ -45,11 +46,12 @@ export default function Home() {
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      const { quantity } = docSnap.data();
+      const {quantity} = docSnap.data();
       if (quantity === 1) {
         await deleteDoc(docRef);
-      } else {
-        await setDoc(docRef, { quantity: quantity - 1 });
+      }
+      else {
+        await setDoc(docRef, {quantity: quantity - 1});
       }
     }
 
@@ -62,19 +64,19 @@ export default function Home() {
   return (
     <Box width="100vw" height="100vh" display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} gap={2}>
       <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
-        <Box position="absolute" top="50%" left="50%" transform="translate(-50%,-50%)" width={400} bgcolor="white" border="2px solid #000" boxShadow={24} p={4} display="flex" flexDirection="column" gap={3} sx={{transform: 'translate(-50%,-50%)'}}>
+        <Box position="absolute" top="50%" left="50%" transform="translate(-50%,-50%)" width={400} bgcolor="white" border="2px solid #0000" boxShadow={24} p={4} display="flex" flexDirection="column" gap={3} sx={{transform: 'translate(-50%,-50%)'}}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
             Add Item
           </Typography>
           <Stack width="100%" direction={'row'} spacing={2}>
-            <TextField id="outlined-basic" label="Item" variant='outlined' fullWidth value={itemName} onChange={(e) => setItemName(e.target.value)} />
-            <Button variant="contained" onClick={() => {
-              addItem(itemName);
-              setItemName('');
-              handleClose();
-            }}>
-              Add
-            </Button>
+            <TextField id="outlined-basic" label="Item" variant='outlined' fullWidth value={itemName} onChange={(e) => setItemName(e.target.value)}/>
+
+            <Button variant="contained" onClick={()=>{
+                addItem(itemName);
+                setItemName('');
+                handleClose();
+              }}
+            >Add</Button>
           </Stack>
         </Box>
       </Modal>
@@ -89,7 +91,7 @@ export default function Home() {
         </Box>
 
         <Stack width="800px" height="300px" spacing={2} overflow={'auto'}>
-          {inventory.length > 0 ? inventory.map(({ name, quantity }) => (
+          {inventory.map(({name, quantity}) => (
             <Box key={name} width="100%" minHeight="150px" display={'flex'} alignItems={'center'} justifyContent={'space-between'} backgroundColor={'#f0f0f0'} paddingX={5}>
               <Typography variant={'h3'} color={'#333'} textAlign={'center'}>
                 {name.charAt(0).toUpperCase() + name.slice(1)}
@@ -101,18 +103,14 @@ export default function Home() {
                 <Button variant="contained" onClick={() => addItem(name)}>
                   Add
                 </Button>
+
                 <Button variant="contained" onClick={() => removeItem(name)}>
                   Remove
                 </Button>
               </Stack>
+
             </Box>
-          )) : (
-            <Box width="100%" minHeight="150px" display={'flex'} alignItems={'center'} justifyContent={'center'}>
-              <Typography variant={'h5'} color={'#333'} textAlign={'center'}>
-                No items in inventory
-              </Typography>
-            </Box>
-          )}
+          ))}
         </Stack>
       </Box>
     </Box>
